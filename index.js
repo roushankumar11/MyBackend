101f751f1ceeb53c4b0ee3d58449d1b27dcca594
roushankumar11 <roushan111204@gmail.com>
2025-09-26 17:23:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 2000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res) =>{
+    res.send('roushandotcom')
+})
+
+app.get('/login', (req,res) =>{
+    res.send('<h1>please login at chai or code</h1>')
+})
+
+app.get('/youtube', (req,res) => {
+    res.send('<h2>Chai or code</h2>')
+})
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
